35b8e1d668bf5b3cfc53329c9dc22d0fc8ac6442
Nhật Quân <80318291+quanyb@users.noreply.github.com>
2023-07-09 08:52:56 +0700

    Create numorder.cpp

---
+++ cyboj/numorder.cpp
@@ -0,0 +1,33 @@
+//numorder
+#include <bits/stdc++.h>
+#define Vani "task"
+long long n, m, k;
+using namespace std;
+
+int cal(long long val){
+    long long pos=0;
+    for(long long i = 1; i <= n; i++){
+        pos += min((long long)sqrt(val - i * i),m);
+        if(val < i * i) break;
+    }
+    return pos;
+}
+
+long long ps(long long l,long long r,int pos){
+    long long m;
+    while(l <= r){
+        m = (l + r) / 2;
+        if (cal(m) < k) l = m + 1;
+        else r = m - 1;
+    }
+    return l;
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("numorder.inp", "r", stdin);
+    freopen("numorder.out", "w", stdout);
+    cin >> m >> n >> k;
+    if(m < n) swap(m, n);
+    cout<<ps(1, m * m + n * n, k);
+}
